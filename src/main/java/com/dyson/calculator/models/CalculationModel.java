66f7de398c88b7928ffea4d99c656944560c7baa
Vimal David Vincent Devaraj <VimalDavid.VincentDevaraj@costain.com>
2020-03-02 08:43:15 +0000

    Removed the change not in scope

--- src/main/java/com/dyson/calculator/models/CalculationModel.java
+++ src/main/java/com/dyson/calculator/models/CalculationModel.java
@@ -4,16 +4,13 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 public class CalculationModel {
 
     @NotNull(message = "Number 1 needs a value")
-    @Size(min = Integer.MIN_VALUE, max = Integer.MAX_VALUE, message = "Number 1 value out of range")
     private @Getter @Setter Integer number1;
 
     @NotNull(message = "Number 2 needs a value")
-    @Size(min = Integer.MIN_VALUE, max = Integer.MAX_VALUE, message = "Number 2 value out of range")
     private @Getter @Setter Integer number2;
 
     private @Getter @Setter CalculationType calculationType;
